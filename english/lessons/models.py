1eec15a28c9c822b7292f4ee0bf53a5c68f5193f
Kostiantyn <k.varetsa@gmail.com>
2024-06-23 22:34:09 +0200

    Created apps models

--- english/lessons/models.py
+++ english/lessons/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Exercise(models.Model):
+    lesson = models.ForeignKey('Lesson', on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+
+    exercise_type_choices = [
+        ('multiple_choice', 'Multiple Choice'),
+        ('fill_in_the_blank', 'Fill in the Blank'),
+        ('true_false', 'True/False'),
+        ('matching', 'Matching'),
+    ]
+    exercise_type = models.CharField(max_length=20, choices=exercise_type_choices)
+
+    description = models.TextField()
+    content = models.TextField()
+    correct_answer = models.CharField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
--- english/users/models.py
+++ english/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to="users/profile_pictures", null=True, blank=True)
+
+
+class UserExerciseAttempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise = models.ForeignKey('lessons.Exercise', on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+    attempt_date = models.DateTimeField(auto_now_add=True)
