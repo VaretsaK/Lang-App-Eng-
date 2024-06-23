6e098ce4bf695c5f24d2d567e32ffa1e95038e2d
Kostiantyn <k.varetsa@gmail.com>
2024-06-23 22:52:59 +0200

    Setup postgresql database with project

--- english/english/settings.py
+++ english/english/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'english.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lang_app_db',
+        'USER': 'lang_app_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
