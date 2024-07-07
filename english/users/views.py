f9eaf42e8a6bfe5c2852bd6062eef474cccaddb6
Kostiantyn <k.varetsa@gmail.com>
2024-07-07 22:26:08 +0200

    Added feat of dynamic form field addition. Modified user/views and user/forms along with signup.html

--- english/users/views.py
+++ english/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from users.forms import LoginForm, UserForm, UserProfileForm
+from users.forms import LoginForm, UserForm, UserProfileForm, PhoneNumberFormSet
 
 from django.contrib import auth, messages
 from django.contrib.auth.models import User
+
+from users.models import UserProfile, PhoneNumber
 
 
 def login(request):
@@ -34,11 +36,14 @@ def logout(request):
 
 
 def signup(request):
+    extra_forms = int(request.GET.get('extra', 1))  # Ensure extra_forms is an integer
+
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST, request.FILES)
+        number_formset = PhoneNumberFormSet(request.POST, queryset=PhoneNumber.objects.none())
 
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid() and profile_form.is_valid() and number_formset.is_valid():
             try:
                 user = User.objects.get(email=user_form.cleaned_data['email'])
                 if user:
@@ -50,6 +55,13 @@ def signup(request):
                 user_profile = profile_form.save(commit=False)
                 user_profile.user = user
                 user_profile.save()
+                print(number_formset)
+
+                for number_form in number_formset:
+                    if number_form.cleaned_data:
+                        phone_number = number_form.save(commit=False)
+                        phone_number.user = user
+                        phone_number.save()
 
                 auth.login(request, user)
                 return redirect('lessons')
@@ -57,7 +69,12 @@ def signup(request):
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
+        number_formset = PhoneNumberFormSet(queryset=PhoneNumber.objects.none())
 
-    return render(request, 'users/signup.html', {'user_form': user_form, 'profile_form': profile_form})
+    return render(request, 'users/signup.html', {'user_form': user_form,
+                                                 'profile_form': profile_form,
+                                                 'number_formset': number_formset,
+                                                 'extra_forms': extra_forms}
+                  )
 
 
